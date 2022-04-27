73c5ce48e074ec2c1476ed6068eca98cd9dd1414
DraxusTDev <draxustienda@gmail.com>
2022-04-27 00:27:38 -0500

    added styles, listenners

--- app.js
+++ app.js
@@ -1,15 +1,40 @@
-/*function player(){
-    const player = prompt("Enter an option: rock, paper or scissors");
-    console.log("Jugador: " + player);
-}
+const id = document.getElementById('cpu');
+const rock = document.getElementById('rock');
+const paper = document.getElementById('paper');
+const scissors = document.getElementById('scissors');
 
-player();*/
+const choise = [];
+
+rock.addEventListener('click', function(e){
+    console.log('rock');
+    if(e.target.dataset.id==='1'){
+        console.log(true);
+        choise.push(1)
+    }else {
+        console.log(false);
+    }
+    
+})
+
+console.log(choise);
+
+paper.addEventListener('click', function(e){
+    console.log('paper')
+    choise = 2;
+    console.log(choise);
+})
+
+scissors.addEventListener('click', function(e){
+    console.log('scissors')
+    choise = 3;
+    console.log(choise);
+})
+
+console.log(choise);
 
 
-
-
-const player = prompt("Enter an option: rock, paper or scissors");
-    console.log("Jugador: " + player);
+//const player = prompt("Enter your choice:");
+ //   console.log("Jugador: " + player);
 
 
 
@@ -24,31 +49,41 @@ function computer(){
     console.log("Número Aleatorio: " + randomNum);
     
     if (randomNum == 1){
-        console.log("Rock");
-        const resPc = "Rock";
-        return resPc;
+        console.log("CPU: Rock");
+        id.innerHTML =`<i class="fas fa-hand-rock"></i>`;
+        //const resPc = "Rock";
+        //return resPc;
     }else if (randomNum == 2){
-        console.log("Paper");
-        const resPc = "Paper";
-        return resPc;
+        console.log("CPU: Paper");
+        id.innerHTML = `<i class="fas fa-hand-paper"></i>`;
+        //const resPc = "Paper";
+        //return resPc;
     } else if (randomNum == 3){
-        console.log("Scissors");
-        const resPc = "Scissors";
-        return resPc;
+        console.log("CPU: Scissors");
+        id.innerHTML =`<i class="fas fa-hand-scissors"></i>`;
+        //const resPc = "Scissors";
+        //return resPc;
     }
-    return
 }
 
 computer();
 
 
-function computerPlay(playerSelection, computerSelection){
+
+if (choise === 1) {
+    console.log("Verdadero");
+}else {
+    console.log("falso");
+}
+
+
+/*function computerPlay(playerSelection, computerSelection){
     console.log("Resultado Jugador: " + playerSelection);
     //console.log("Resultado computadora: " +computerSelection);
 }
 
 
-computerPlay(player);
+computerPlay(player);*/
 
 
 
